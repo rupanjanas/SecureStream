89152165f0574d291b09aa221dd57e75166e3512
rupanjanas <saharupanjana04@gmail.com>
2026-05-31 19:00:02 +0530

    Update Dashboard.jsx

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useState, useLocation} from "react";
+import { useEffect, useState} from "react";
+import { useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import { getHealth, listDocuments, getSession } from "../api/aiService";
